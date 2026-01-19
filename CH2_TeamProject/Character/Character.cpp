4ab99349be090276af82133e82c9be87837d2d68
ssh1202 <tmdghks9812@gmail.com>
2026-01-19 14:30:46 +0900

    캐릭터

--- CH2_TeamProject/Character/Character.cpp
+++ CH2_TeamProject/Character/Character.cpp
@@ -1,45 +1,35 @@
-// 파일명: Character.cpp
 #include "Character.h"
+#include <iostream>
+#include <string>
 
-// ---------------------------------------------------------
-// [Mission 4] 생성자에서 기본값 설정하기
-// ---------------------------------------------------------
 ACharacter::ACharacter()
 {
-    // Q1. Name 변수에 "Unknown"을 넣어주세요. (문자열은 큰따옴표 "")
-    // (여기에 작성)
 
-    // Q2. Hp는 100, Atk는 10으로 설정해주세요.
-    // (여기에 작성)
+    Name = "Unkown";
 
-    // (여기에 작성)
+    Hp = 10;
 
+    Atk = 10;
 
-    cout << "ACharacter 생성됨: " << Name << " (HP: " << Hp << ")" << endl;
+    std::cout << "ACharacter 생성됨: " << Name << " (HP: " << Hp << ")" << std::endl;
 }
 
 ACharacter::~ACharacter()
 {
-    cout << "ACharacter 소멸됨" << endl;
+    std::cout << "ACharacter 소멸됨" << std::endl;
 }
 
-// ---------------------------------------------------------
-// [Mission 5] 행동 구현하기
-// ---------------------------------------------------------
 
 void ACharacter::Attack()
 {
-    // Q3. "OOO가 공격합니다! (공격력: XX)" 형식으로 로그를 출력해보세요.
-    // 힌트: cout << Name << "가 공격... " << Atk << ... ;
-    // (여기에 작성)
 
+    std::cout << Name << "가 공격합니다! (공격력" << Atk << ")" << std::endl;
 }
 
 void ACharacter::TakeDamage(int DamageAmount)
 {
-    // Q4. 현재 체력(Hp)에서 들어온 데미지(DamageAmount)만큼 빼주세요.
-    // (여기에 작성)
 
+    Hp -= DamageAmount;
 
     cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << endl;
 }
---
+++ CH2_TeamProject/Main.cpp
@@ -0,0 +1,18 @@
+#include "Character/Character.h"
+
+int main()
+{
+    // 1. 캐릭터 생성
+    ACharacter* MyChar = new ACharacter();
+
+    // 2. 공격 테스트
+    MyChar->Attack();
+
+    // 3. 피격 테스트 (데미지 30을 줘봅시다)
+    MyChar->TakeDamage(30);
+
+    // 4. 삭제
+    delete MyChar;
+
+    return 0;
+}
